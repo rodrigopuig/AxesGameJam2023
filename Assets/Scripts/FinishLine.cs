d05aecc3e70500e71df431cb3e600e6ddf6a3f0f
Daniel Fernandez <daniFMdev@gmail.com>
2023-05-27 15:33:14 +0200

    finish line

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    public GameObject trigger1;
+    public GameObject trigger2;
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger2.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(count == 0)
+        {
+        trigger1.SetActive(false);
+        trigger2.SetActive(true);
+        }
+        else if(count == 1)
+        {
+            trigger2.SetActive(false);
+        }
+        count++;
+    }
+}
