032aac6e50819760a2d5066a8afe68ec5b9a9a7b
Daniel Fernandez <daniFMdev@gmail.com>
2023-05-28 13:29:33 +0200

    key group

---
+++ Assets/Scripts/UI/Control/KeyGroup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class KeyGroup : MonoBehaviour
+{
+    public TextMeshProUGUI key1;
+    public TextMeshProUGUI key2;
+    public Image sprite;
+    public LeftRightSlider lrSlider;
+    public NormalSlider normalSlider;
+}
